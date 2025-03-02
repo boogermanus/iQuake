b864789503fe1433bd147f2192fb8426c649aa75
Jordan Woodruff <boogermanus@gmail.com>
2025-03-02 13:43:47 -0600

    markers working

--- iQuake/src/app/components/map/map.component.ts
+++ iQuake/src/app/components/map/map.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { LeafletModule } from '@bluehalo/ngx-leaflet';
-import { LatLng, latLng, LeafletMouseEvent, tileLayer } from 'leaflet';
+import { LatLng, latLng, Layer, LeafletMouseEvent, tileLayer, marker, icon } from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -14,11 +14,11 @@ import { LatLng, latLng, LeafletMouseEvent, tileLayer } from 'leaflet';
   styleUrl: './map.component.scss'
 })
 export class MapComponent {
-
+  public markers: Layer[] = [];
   public latLng!: LatLng
   public options: any = {
     layers: [
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: 'Open Street Map' })
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: 'Open Street Map' }),
     ],
     zoom: 5,
     center: latLng(33.67, -101.82)
@@ -26,5 +26,8 @@ export class MapComponent {
 
   public handleClick(event: LeafletMouseEvent): void {
     this.latLng = event?.latlng;
+    this.markers.splice(0);
+    const newMarker = marker([this.latLng.lat, this.latLng.lng], {icon: icon({iconUrl: 'assets/marker-icon.png'})})
+    this.markers.push(newMarker);
   }
 }
