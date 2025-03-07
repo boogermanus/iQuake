16c6121b21e950da627b458b812348b8b8f3c576
Jordan Woodruff <boogermanus@gmail.com>
2025-03-07 13:50:54 -0600

    initial migration

--- dotnet/iQuake.Api/Program.cs
+++ dotnet/iQuake.Api/Program.cs
@@ -1,3 +1,7 @@
+using iQuake.Core.Models.Database;
+using iQuake.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://localhost:5006");
 builder.Services.AddControllers();
@@ -6,6 +10,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// database setup
+var connStr = builder.Configuration.GetConnectionString("DefaultConnection") ??
+    throw new Exception("Connection string 'DefaultConnection' not found");
+builder.Services.AddDbContext<iQuakeDbContext>(options => options.UseSqlite(connStr));
+builder.Services.AddDefaultIdentity<User>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+    options.Password.RequireDigit = false;
+})
+.AddEntityFrameworkStores<iQuakeDbContext>();
 
 var app = builder.Build();
 
