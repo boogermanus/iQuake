01338972e5e964fb8586599a1c9b74f7cf6d444a
Jordan Woodruff <jwoodruff@datascan.com>
2025-10-21 17:12:55 -0500

    Close #3

--- iQuake/src/app/components/quakes/quakes.component.ts
+++ iQuake/src/app/components/quakes/quakes.component.ts
@@ -1,32 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { QuakeService } from '../../services/quake.service';
-import { DataService } from '../../services/data.service';
-import { ILocation } from '../../interfaces/ilocation';
-import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
-import { LatLng } from 'leaflet';
-import { Router } from '@angular/router';
+import {Component, inject, OnInit} from '@angular/core';
+import {QuakeService} from '../../services/quake.service';
+import {DataService} from '../../services/data.service';
+import {ILocation} from '../../interfaces/ilocation';
+import {CommonModule} from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {LatLng} from 'leaflet';
+import {Router} from '@angular/router';
 
 @Component({
-    selector: 'app-quakes',
-    imports: [
-        CommonModule,
-        MatCardModule
-    ],
-    templateUrl: './quakes.component.html',
-    styleUrl: './quakes.component.scss'
+  selector: 'app-quakes',
+  imports: [
+    CommonModule,
+    MatCardModule
+  ],
+  templateUrl: './quakes.component.html',
+  styleUrl: './quakes.component.scss'
 })
 export class QuakesComponent implements OnInit {
 
   public location?: ILocation = undefined;
   public features: any[] = [];
   public noQuakes: boolean = false;
-  constructor(
-    private readonly quakeService: QuakeService,
-    private readonly dataService: DataService,
-    private readonly router: Router
-  ) {
+  private readonly quakeService: QuakeService = inject(QuakeService);
+  private readonly dataService: DataService = inject(DataService);
+  private readonly router: Router = inject(Router);
 
+  constructor() {
   }
 
   public ngOnInit(): void {
@@ -37,23 +36,39 @@ export class QuakesComponent implements OnInit {
 
     if (this.location !== undefined) {
       this.loadQuakes();
-    }
-    else {
+    } else {
       this.router.navigate(['/location']);
     }
 
   }
 
   public loadQuakes() {
-    this.quakeService.getQuakes(this.location ?? { latLng: new LatLng(0, 0), mag: 0, range: 250 })
+    this.quakeService.getQuakes(this.location ?? {latLng: new LatLng(0, 0), mag: 0, range: 250})
       .subscribe({
         next: (data) => {
           this.features = data.features
-          if(this.features.length === 0) {
+          if (this.features.length === 0) {
             this.noQuakes = true;
           }
         }
       })
   }
 
+  public getMagnitudeStyle(magnitude: number): string {
+    let result = '';
+
+    if (magnitude <= 2.5) {
+      result = 'color: green;'
+    } else if (magnitude <= 4.5) {
+      result = 'color: blue;';
+    }
+    else if (magnitude <= 5) {
+      result = 'color: orange;';
+    }
+    else {
+      result = 'color: red;';
+    }
+
+    return result;
+  }
 }
